60bd01bfc3bfc0050bcd0634ada5f27543f96dae
Jonathan Finger <jaefinger@gmail.com>
2023-08-22 19:12:34 -0500

    updated app to base code from docs

---
+++ app/main.py
@@ -0,0 +1,60 @@
+# from fastapi import FastAPI
+# from google.oauth2.credentials import Credentials
+# from googleapiclient.discovery import build
+
+# import os
+# from dotenv import load_dotenv
+
+# # Get environment variables
+# load_dotenv()
+# SPREADSHEET_ID = os.getenv("YOUR_SPREADSHEET_ID")
+
+# # Initialize the FastAPI app
+# app = FastAPI()
+
+
+# @app.post("/submit")
+# def submit(name: str, email: str):
+#     # Load the credentials from the `token.json` file created during the Sheets API setup
+#     creds = Credentials.from_authorized_user_file(
+#         "token.json", ["https://www.googleapis.com/auth/spreadsheets"]
+#     )
+
+#     # Connect to the Sheets API
+#     service = build("sheets", "v4", credentials=creds)
+#     sheet = service.spreadsheets()
+
+#     # Data to append
+#     values = [[name, email]]
+#     body = {"values": values}
+
+#     # Append the data to the sheet
+#     result = (
+#         sheet.values()
+#         .append(
+#             spreadsheetId=SPREADSHEET_ID,
+#             range="Sheet1!A1:B1",
+#             valueInputOption="RAW",
+#             body=body,
+#         )
+#         .execute()
+#     )
+
+#     return {"message": "Data added to the sheet"}
+
+
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
