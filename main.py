029c0b9f0c4effcb727d3d06e505629196434c1a
Jonathan Finger <jaefinger@gmail.com>
2023-08-24 23:46:22 -0500

    tested baby form

--- main.py
+++ main.py
@@ -20,33 +20,59 @@ def read_root():
 # Load google sheet based information
 # get env variable called 'GOOGLE_CREDENTIAL_DICT'
 GOOGLE_CREDENTIAL_DICT = ast.literal_eval(os.getenv("GOOGLE_CREDENTIAL_DICT"))
-CONTACT_SPREADSHEET_ID = os.getenv("CONTACT_SPREADSHEET_ID")
+GOOGLE_SPREADSHEET_ID = os.getenv("GOOGLE_SPREADSHEET_ID")
 
 # Authenticate with Google
 gc = gspread.service_account_from_dict(GOOGLE_CREDENTIAL_DICT)
-sh = gc.open_by_key(CONTACT_SPREADSHEET_ID)
+sh = gc.open_by_key(GOOGLE_SPREADSHEET_ID)
+volunteer_worksheet = sh.sheet1
 
 
-# Data model for the request
-class UserData(BaseModel):
-    name: str
+class VolunteerForm(BaseModel):
+    name_first: str
+    name_middle: str
+    name_last: str
     email: str
+    primary_phone: str
+    street_address: str
+    city: str
+    state_province_region: str
+    postal_zip_code: str
+    country: str
 
 
-@app.post("/write_user_data")
-async def write_user_data(user_data: UserData):
-    """
-    Writes name and email to a Google Sheet
-    - name: User's name 📝
-    - email: User's email 📝
-    """
+@app.post("/submit_volunteer_form")
+async def submit_volunteer_form(volunteer_form: VolunteerForm):
+    # extract data from volunteer_form
+    name_first = volunteer_form.name_first
+    name_middle = volunteer_form.name_middle
+    name_last = volunteer_form.name_last
+    email = volunteer_form.email
+    primary_phone = volunteer_form.primary_phone
+    street_address = volunteer_form.street_address
+    city = volunteer_form.city
+    state_province_region = volunteer_form.state_province_region
+    postal_zip_code = volunteer_form.postal_zip_code
+    country = volunteer_form.country
 
-    # Find the index of the "Name" and "Email" columns
-    name_index = sh.row_values(1).index("Name") + 1
-    email_index = sh.row_values(1).index("Email") + 1
+    # add all of these to a new row in volunteer_worksheet
+    volunteer_worksheet.append_row(
+        [
+            name_first,
+            name_middle,
+            name_last,
+            email,
+            primary_phone,
+            street_address,
+            city,
+            state_province_region,
+            postal_zip_code,
+            country,
+        ]
+    )
 
-    # Append the name and email to the respective columns
-    sh.update_cell(sh.row_count + 1, name_index, user_data.name)
-    sh.update_cell(sh.row_count + 1, email_index, user_data.email)
 
-    return {"message": "Name and email written successfully!"}
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
