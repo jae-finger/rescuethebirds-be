bfbe8a07ccdd51db4c976b2d88e54b648d708e35
Jonathan Finger <jaefinger@gmail.com>
2023-08-25 19:26:40 -0500

    hiding ping route

--- app/api/ping.py
+++ app/api/ping.py
@@ -5,8 +5,8 @@ from app.config import Settings, get_settings
 router = APIRouter()
 
 
-@router.get("/ping")
-async def pong(settings: Settings = Depends(get_settings), include_in_schema=False):
+@router.get("/ping", include_in_schema=False)
+async def pong(settings: Settings = Depends(get_settings)):
     return {
         "ping": "pong!",
         "environment": settings.environment,
