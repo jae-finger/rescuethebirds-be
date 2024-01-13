90b5655da7b23a794f4080bad8175798e4c6b75b
Jon Finger <jaefinger@gmail.com>
2024-01-13 15:57:56 -0600

    updated pytest code for main

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,9 +1,15 @@
 from fastapi.testclient import TestClient
 from ..main import app  # Relative import of the FastAPI app
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+code_environment = os.getenv("CODE_ENVIRONMENT")    
 
 client = TestClient(app)
 
 def test_read_main():
     response = client.get("/ping")
     assert response.status_code == 200
-    assert response.json() == {"ping": "pong!", "environment": "dev"} 
+    assert response.json() == {"ping": "pong!", "environment": code_environment} 
